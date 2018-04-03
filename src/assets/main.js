889d834c175db1a8a0d76a59f7e1db60d83fe720
Cheniece Manning <cheniecemanning@hotmail.com>
2018-04-03 15:20:24 +0100

    Switch statments for answer length

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,7 +8,30 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields(){
-    answer = Math.floor(Math.random() * 10000);
+    answer = Math.floor(Math.random() * 10000).toString;
+
+    switch(answer.value.length <= 4){
+        case answer.value.length === 3:
+         answer.value += "0" + answer
+         break;
+
+        case answer.value.length === 2:
+         answer.value += "00" + answer
+         break;
+
+         case answer.value.length === 1:
+         answer.value += "000" + answer
+         break;
+
+         default:
+            answer.value ==  answer;
+
+    }
+
+
     attempt = 0;
 
+
 }
+
+console.log(setHiddenFields());
